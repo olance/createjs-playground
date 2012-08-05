0bf5e2d1c747f3cf4ccc1ecccac4a80739d55ef9
Olivier Lance <olivier@sylights.com>
2012-08-06 00:01:22 +0200

    Reorganized files + extracted CSS and Javascript from sample 2 HTML file to separate css/js files

---
+++ demos/tweenjs-sample2/js/main.js
@@ -0,0 +1,61 @@
+(function ($) {
+
+    var $canvas = null;
+    var stage = null;
+
+    function updateCanvasSize() {
+        $canvas.attr("width", $(window).outerWidth());
+        $canvas.attr("height", $(window).outerHeight());
+    }
+
+    $(document).ready(function () {
+        $canvas = $("#stage");
+        updateCanvasSize();
+
+        // Create new EaselJS stage from canvas
+        stage = new Stage($canvas[0]);
+
+
+        // Create our circles compound object (see js/circles.js) and add it to the stage
+        var circles = new playground.Circles(25);
+        stage.addChild(circles);
+
+        // Position the description div as an EaselJS DOM element
+        var $desc = $("#description");
+        var desc = new DOMElement($desc[0]);
+        desc.x = 30;
+        desc.y = 30;
+        desc.alpha = 0.75;
+        stage.addChild(desc);
+
+        stage.onMouseUp = function (event) {
+            circles.moveTo(stage.mouseX, stage.mouseY);
+
+            // Just for fun — Move the description to the other side of the screen when
+            // the user set the circles center below the div
+            var pos = $desc.position();
+            var dim = { width:$desc.outerWidth(), height:$desc.outerHeight() };
+            if (stage.mouseX >= pos.left && stage.mouseX <= pos.left + dim.width &&
+                stage.mouseY >= pos.top && stage.mouseY <= pos.top + dim.height) {
+                var to = {x:30, y:30};
+                if (pos.left === 30) {
+                    to = {
+                        x:$(window).outerWidth() - dim.width - 30,
+                        y:$(window).outerHeight() - dim.height - 30
+                    }
+                }
+
+                Tween.get(desc, { override:true }).to(to, 500, Ease.cubicInOut);
+            }
+        };
+
+        // Set the stage as listener on the Ticker, so that stage.update() is called periodically
+        Ticker.addListener(stage);
+    });
+
+    $(window).resize(function () {
+        updateCanvasSize();
+        stage.update();
+    });
+
+})(jQuery);
