eb1720cc1aaab4cb5c30b1b16186792362c75f06
Olivier Lance <olivier@sylights.com>
2012-09-02 21:46:07 +0200

    Added a real simple location hash monitoring to select the proper demo from the URL fragment

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,14 @@
         $playground.height($(window).outerHeight() - $header.outerHeight() - $footer.outerHeight());
     }
 
+	function openDemoFromHash() 
+	{
+		if(window.location.hash.length > 0)
+		{
+			$(window.location.hash).click();
+		}
+	}
+	
     $(document).ready(function() {
         updatePlaygroundDimensions();
         $(window).resize(updatePlaygroundDimensions);
@@ -15,6 +23,18 @@
         $("#header #menu li a").click(function() {
             $("#header #menu li").removeClass("active");
             $(this).parent().addClass("active");
+	        
+	        $("#" + $(this).attr("target")).attr("src", $(this).attr("href"));
+	        window.location.hash = "#" + $(this).attr("id");
+	        
+	        return false;
         });
+	    
+	    // A *very* simple URL fragment "management" to open the correct sample from window.location
+	    openDemoFromHash();
+	    if("onhashchange" in window)
+	    {
+		    window.onhashchange = openDemoFromHash;
+	    }
     });
 }(jQuery));
